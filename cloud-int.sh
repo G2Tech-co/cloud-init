24e20d40e0fc97ae927efa64a290d441369092ce
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-01-07 13:46:21 +0330

    update order

--- cloud-int.sh
+++ cloud-int.sh
@@ -22,17 +22,17 @@ apt update && apt upgrade -y
 apt install git
 sleep 1
 
-# install gitlab-runner
-mkdir runner && cd runner
-curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_${arch}.deb"
-dpkg -i gitlab-runner_<arch>.deb
-sleep 1
-
 # install docker
 sudo apt-get install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
+
+# install gitlab-runner
+mkdir runner && cd runner
+curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+dpkg -i gitlab-runner_amd64.deb
+sleep 1
