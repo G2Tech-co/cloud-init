6628fc6efa55cc281a6c5fb4f37dbd5f7d8c6c13
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-01-08 11:57:50 +0330

    change order

--- hetzner.sh
+++ hetzner.sh
@@ -28,8 +28,8 @@ runcmd:
   - sed -i -e '/^#AllowTcpForwarding/s/^.*$/AllowTcpForwarding no/' /etc/ssh/sshd_config
   - sed -i -e '/^#AllowAgentForwarding/s/^.*$/AllowAgentForwarding no/' /etc/ssh/sshd_config
   - sed -i -e '/^#AuthorizedKeysFile/s/^.*$/AuthorizedKeysFile .ssh\/authorized_keys/' /etc/ssh/sshd_config
-  - echo "Installing docker..."
   - sed -i '$a AllowUsers arsalan' /etc/ssh/sshd_config
+  - echo "Installing docker..."
   - mkdir -p /etc/apt/keyrings
   - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
