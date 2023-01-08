ac28ad60b477376e910cb7e957ca0330cfc33b9d
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-01-08 15:10:34 +0330

    docker to runner

--- hetzner.sh
+++ hetzner.sh
@@ -38,4 +38,5 @@ runcmd:
   - echo "Installing gitlab-runner..."
   - curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
   - dpkg -i gitlab-runner_amd64.deb
+  - sudo usermod -aG docker gitlab-runner
   - reboot
